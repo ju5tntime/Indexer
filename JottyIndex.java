feac6027a7a597d38dcd23599810a6c71fb1e347
jotty182 <53579177+jotty182@users.noreply.github.com>
2019-08-02 15:47:46 -0600

    Working scan / listarray loop
    the loop works right. End is no longer in the arraylist, and it seems like i've got some good logic happening here. nothing hacky.

--- JottyIndex.java
+++ JottyIndex.java
@@ -3,13 +3,24 @@ import java.util.*;
 public class JottyIndex {
 	public static void main(String args[]) {
 
-		String nextIndex = null;
+		String nextIndex = "empty";
+		boolean control = true;
 		Scanner scan = new Scanner(System.in);
 		ArrayList<String> list = new ArrayList<String>();
 
-		System.out.println("Enter something:");
-		nextIndex = scan.nextLine();
-		System.out.println("You entered: " + nextIndex);
+		while(control) {
+			System.out.println("Lets add another stupid index! Gimme it:");
+			if(!nextIndex.equals("end")) {
+				nextIndex = scan.nextLine();
+				list.add(nextIndex);
+			} else {
+				list.remove("end");
+				break;
+			}
+			
+		}
+		System.out.println("The array has " + list + " in it. Yay.");
+		System.out.println("There are " + list.size() + " number's in the array.");
 
 	}
 }
