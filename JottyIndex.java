f51698ce724c81a47741fa3ef97988c31a6a9f4f
jotty182 <53579177+jotty182@users.noreply.github.com>
2019-08-02 16:30:00 -0600

    added an import
    ineeded to create and write to files using either PrintWriter or FIle??

--- JottyIndex.java
+++ JottyIndex.java
@@ -1,12 +1,17 @@
 import java.util.*;
+import java.io.*; // this has PrintWriter and File in it
 
 public class JottyIndex {
 	public static void main(String args[]) {
 
+		// vars
 		String nextIndex = "empty";
 		boolean control = true;
+	
+		// utilities
 		Scanner scan = new Scanner(System.in);
 		ArrayList<String> list = new ArrayList<String>();
+		
 
 		while(control) {
 			System.out.println("Lets add another stupid index! Gimme it:");
@@ -20,7 +25,7 @@ public class JottyIndex {
 			
 		}
 		System.out.println("The array has " + list + " in it. Yay.");
-		System.out.println("There are " + list.size() + " number's in the array.");
+		System.out.println("There are " + list.size() + " indices in the array.");
 
 	}
 }
